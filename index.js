34ac3f54375365900c540e23c68ca3ed63f0adc2
Tom Harrop <tom@tomharrop.com>
2015-09-02 16:23:38 +0100

    initial

--- index.js
+++ index.js
@@ -15,9 +15,8 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  var id = ws.send(JSON.stringify(new Date()), function() {  })
+
 
   console.log("websocket connection open")
 
